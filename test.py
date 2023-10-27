58eb9abefd57be42879546f423b093d8ba0701d5
“@Kevine12” <ikevine2016@gmail.com>
2023-10-27 13:16:54 +0200

    adding data

--- test.py
+++ test.py
@@ -145,3 +145,20 @@ news_ticker = news_ticker[:140]
 
 # Print the resulting news ticker
 print(news_ticker)
+
+# Prime numbers are whole numbers that have only two factors: 1 and the number itself. The first few prime numbers are 2, 3, 5, 7.
+
+# For instance, 6 has four factors: 1, 2, 3, 6.
+
+# 1 X 6 = 6
+
+# 2 X 3 = 6
+
+# So we know 6 is not a prime number.
+
+# This practice is not graded. Using the workspace on the Coding Space page, write code to check if the numbers provided in the list check_prime are prime numbers.
+
+# If the numbers are prime, the code should print "[number] is a prime number."
+# If the number is NOT a prime number, it should print "[number] is not a prime number", and a factor of that number, other than 1 and the number itself: "[factor] is a factor of [number]".
+
+# 7 IS a prime number 26 is NOT a prime number, because 2 is a factor of 26
