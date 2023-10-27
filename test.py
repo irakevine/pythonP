dd44ebf4203122b7f7cb647656f2f991332749d4
“@Kevine12” <ikevine2016@gmail.com>
2023-10-27 13:59:55 +0200

    :adding data

--- test.py
+++ test.py
@@ -212,3 +212,13 @@ for actor in cast:
 # Print the names and heights tuples
 print("Names:", names)
 print("Heights:", heights)
+
+# Use zip to transpose data from a 4-by-3 matrix to a 3-by-4 matrix. There's actually a cool trick for this! Feel free to look at the solutions if you can't figure it out.
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+# Use the zip function with the * operator to transpose the data
+data_transpose = tuple(zip(*data))
+
+# Print the transposed data
+for row in data_transpose:
+    print(row)
