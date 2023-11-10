993b04cdcf4f3a0818b9c6ca2c67c5458fb1d5f1
“@Kevine12” <ikevine2016@gmail.com>
2023-11-10 09:32:44 +0200

    adding numpy with how can it used

--- test.py
+++ test.py
@@ -420,3 +420,12 @@ with open("camelot.txt") as song:
 # start = time.time()
 # np.mean(x)
 # print(time.time() - start)
+
+## We import NumPy into Python
+# import numpy as np
+
+# ## We create a 1D ndarray that contains only integers
+# x = np.array([1, 2, 3, 4, 5])
+
+# ## Let's print the ndarray we just created using the print() command
+# print('x = ', x)
