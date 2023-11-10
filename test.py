5b12fd16640cd553e7ab3fb7ad75e02c1f4abe6d
“@Kevine12” <ikevine2016@gmail.com>
2023-11-10 09:43:15 +0200

    adding numpy with how can it used

--- test.py
+++ test.py
@@ -528,3 +528,8 @@ print('The elements in Y are of type:', Y.dtype)
 # [ 4 5 6]
 # [ 7 8 9]
 # [10 11 12]]
+
+# Y has dimensions: (4, 3)
+# Y has a total of 12 elements
+# Y is an object of type: class 'numpy.ndarray'
+# The elements in Y are of type: int64
